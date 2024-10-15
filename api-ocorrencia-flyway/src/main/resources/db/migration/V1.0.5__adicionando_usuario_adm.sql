e7a8fae142c25a0614953ed37f3a4e9f49337e96
Danieloliver11 <dmoliveira@apoioprodesp.sp.gov.br>
2024-10-15 20:04:05 -0300

    alterando arquivo no flyway

--- api-ocorrencia-flyway/src/main/resources/db/migration/V1.0.5__adicionando_usuario_adm.sql
+++ api-ocorrencia-flyway/src/main/resources/db/migration/V1.0.5__adicionando_usuario_adm.sql
@@ -1,5 +1,5 @@
 INSERT INTO cliente (cod_cliente, nme_cliente, dt_nascimento, nmo_cpf, dt_criacao, senha, roles)
-SELECT nextval('cliente_seq'), 'Mario Vitar', '1995-01-01', '50398963010', '2024-10-13', 'root', 1
+SELECT nextval('cliente_seq'), 'Mario Vitar', '1995-01-01', '50398963010', '2024-10-13', '$2a$10$by5I.UV11M75no8WDoHaSuao2nRvgpD8dWCbVDzeLpm4Cl/C0ZqYe', 1
 WHERE NOT EXISTS (
     SELECT 1 FROM cliente WHERE nmo_cpf = '50398963010'
 );
